4ad21034ca58d01318de8703689b064b5adfd859
DarioSuarezL <dsuarezlazarte@gmail.com>
2024-05-14 23:15:03 -0400

    Endpoint para crear coordenadas - test

---
+++ app/Http/Controllers/CoordenadaController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Coordenada;
+use Illuminate\Http\Request;
+
+class CoordenadaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $coord = Coordenada::create($request->all());
+
+        if ($coord) return response()->json(['message' => 'Coordenada creada correctamente'], 201);
+
+        return response()->json(['message' => 'Error al crear la coordenada'], 500);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
